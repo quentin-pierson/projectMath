50f99beeb7ce265e9eef48b2178a8faf0c67ff9a
quentin-pierson <quentinprs62@gmail.com>
2022-01-11 12:40:44 +0100

    add some comment

--- exercice7.R
+++ exercice7.R
@@ -1,6 +1,11 @@
+#install.packages("corrplot")
+#install.packages("factoextra")
+
+library(factoextra)
+library(corrplot)
+
 df <- read.csv("données.csv", header=TRUE, sep=';', row.names="X")
 head(df)
-
 
 names(df)
 
@@ -10,11 +15,36 @@ head(corre)
 
 
 # Question 2
+# Sortir une meilleur matrice 
 zdf <- as.data.frame(as.table(corre))
 zdf <-zdf[order(-zdf$Freq),]
 zdf
 
 
+# Question 3 
+# Sortir la matrice des composants pricniaples et prendre les plus importants
+# calculer la part d'inertie en prenant les deux pourcentages des principaux components 
+
+# ACP
+res.pca <- prcomp(df, scale = TRUE)
+fviz_eig(res.pca)
+fviz_pca_ind(res.pca,
+             col.ind = "cos2", # Color by the quality of representation
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+
+fviz_pca_var(res.pca,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+
+
+
 # Question 5
 library(corrplot)
 corrplot(corre, method="circle")
+
+
+
